d77e4e3f07045f185e317e692da29d66848746fd
shirne <shirne@126.com>
2024-01-08 11:21:28 +0800

    fix duplicate logger listener

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 0.0.7
+* fix duplicate logger listener
+
 ## 0.0.6
 * improve logger
 
--- lib/src/tools.dart
+++ lib/src/tools.dart
@@ -2,39 +2,49 @@ import 'dart:developer';
 
 import 'package:logging/logging.dart';
 
-/// override logger level
-Level loggerLevel = Level.ALL;
-
 /// package log
-final logger = Logger('CCHESS')
-  ..onRecord.listen((record) {
-    if (loggerLevel.value > record.level.value) {
-      return;
-    }
-    log(
-      record.message,
-      time: record.time,
-      level: record.level.value,
-      error: record.error,
-      stackTrace: record.stackTrace,
-      sequenceNumber: record.sequenceNumber,
-    );
-  });
+final logger = _getLogger('CCHESS');
+
+/// override logger level
+Level _loggerLevel = Level.ALL;
+
+bool _loggerListened = false;
+
+Logger _getLogger(String name) {
+  final logger = Logger('CCHESS');
+  if (!_loggerListened && hierarchicalLoggingEnabled) {
+    _loggerListened = true;
+    logger.onRecord.listen((record) {
+      if (_loggerLevel.value > record.level.value) {
+        return;
+      }
+      log(
+        record.message,
+        time: record.time,
+        level: record.level.value,
+        error: record.error,
+        stackTrace: record.stackTrace,
+        sequenceNumber: record.sequenceNumber,
+      );
+    });
+  }
+  return logger;
+}
 
 /// control package
 class CChess {
   /// close log of cchess package
   static void closeLog() {
-    loggerLevel = Level.OFF;
+    _loggerLevel = Level.OFF;
   }
 
   /// default to log info level
   static void openLog() {
-    loggerLevel = Level.INFO;
+    _loggerLevel = Level.INFO;
   }
 
   /// set log level of cchess package
   static set logLevel(Level level) {
-    loggerLevel = level;
+    _loggerLevel = level;
   }
 }
